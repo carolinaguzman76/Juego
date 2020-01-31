d4e23354ab26466f8c823103c34afa9eb95b90e5
Carolina Guzman <carolinaguzman76@gmail.com>
2020-01-31 13:59:38 +0100

    Añadido instrucciones

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 window.onload = () => {
     document.getElementById("myCanvas").style.display = "none"
     document.getElementById("btnStart").onclick = () => {
+        document.getElementById("space").style.display = "none"
         document.getElementById("startGame").style.display = "none"
         document.getElementById("myCanvas").style.display = "block"
         game.init('myCanvas')
